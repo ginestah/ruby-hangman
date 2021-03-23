427512f5e43ad8bd59ff1a706bb3f93b3899161e
ginestah <68362184+ginestah@users.noreply.github.com>
2021-03-22 21:56:12 -0400

    added a word bank file, and updated game to grab from file, option to restart game, shows letters you have guessed so far

--- hangman.rb
+++ hangman.rb
@@ -1,17 +1,18 @@
+file = File.open("words.txt")
+# file_data = file.read
+file_data = file.readlines.map(&:chomp)
+$file_data = file_data.reject{|c| c.empty?}
+$guessed_letters = []
+
 class Word
-  # Stores the provided word in an instance variable
   def initialize(word)
-    # split the mystery word into an array of letters and save to an instance variable
 @array_of_letters = word.split("")
-    # map through the letters array, replacing each letter with an underscore.
    @array_of_underscore = @array_of_letters.map{ |i| i="_"}
-    # this will represent the displayed letters save this to an instance variable.
   end
  
   
   def guess?(letter)
-    # use conditional logic to check whether
-    # the array of letters includes the guessed letter.
+    
 if(@array_of_letters.include? letter)
   @array_of_letters.each_with_index do |each_letter,index|
     if  each_letter == letter
@@ -23,18 +24,7 @@ if(@array_of_letters.include? letter)
 else
   pp 'you were wrong'
   return false
-end
-    # if it does, inform the user that they were right
-    # and this .guess? method should return true. 
-    # if not, inform the user that they're wrong
-    # and have the .guess? method return false.
-
-
-    # loop through the letters array with index. 
-    # if the guessed letter matches a character, 
-    # replace the underscore at that index with the guessed letter.
-   
-    # *** REMINDER: ? methods must return a boolean ***
+      end
   end
   
   def guessed_all_correct?
@@ -45,60 +35,61 @@ end
       false
     end
   
-    # Check whether all letters in the word have been guessed correctly.
   end
 
   def output_word
-    pp @array_of_underscore.join
-    # puts @array_of_letters.join
-    # Display the current state of the guessed word for the player.
+    pp "The word currently looks like: #{@array_of_underscore.join}"
+  end
+  def solution
+    pp "You lose the word was #{@array_of_letters.join}"
   end
 end
 
 class MysteryWordGame
   def initialize
-  # Initialize the amount of lives, the ask_for_word method, and the game loop.
-  @lives = 5
+  # Initialize the amount of lives and the game loop.
+  @lives = 6
   ask_for_word
   game_loop
   end
 
   def ask_for_word
-  pp "What is the mystery word?"
-  mystery_word = gets.chomp
+  mystery_word = $file_data.sample
   @new_word= Word.new(mystery_word)
-  # ask the user for a mystery word and save it to an instance variable 
+  # grab a mystery word from txt file and and save it to an instance variable 
   end
 
   def game_loop
-    # Run the game loop until the player wins or loses.
+    # Run the game loop until the player wins or loses, with the option to play again
     loop do
       # show the current output of the word
       @new_word.output_word
       # inform the user how many lives they have left
 puts "you have #{@lives} lives left"
-      # ask the user to guess a letter and save their guess to a local variable
-puts "Guess a letter"
+puts "Guess a letter or -"
 @letter =  gets.chomp
-
-      # (BONUS) Check whether a provided character is a letter
-      # (BONUS) check and  ensure the user only entered one letter    
-
-      # use the .guess?(letter) method to check if the user's guess was correct
-      # save the result to an instance variable
+$guessed_letters.push @letter
+puts "letters you have guessed so far: #{$guessed_letters.join}"
 @result = @new_word.guess?(@letter)
-if(@result == false)
-  pp  'You lost a life'
-  @lives = @lives -1
-end
-      # if the guess was wrong, the player loses a life
+
+@result?nil:@lives = @lives-1
 if(@lives == 0)
-  pp 'you lost the game'
+ @new_word.solution
+ puts "play again yes/no?"
+ @answer = gets.chomp
+ if(@answer == 'yes')
+   MysteryWordGame.new
+ end
   break
 end
 
 if(@new_word.guessed_all_correct?)
 'you win!'
+puts "play again? yes/no"
+@answer = gets.chomp
+if(@answer == 'yes')
+  MysteryWordGame.new
+end
 break
 end
     end
